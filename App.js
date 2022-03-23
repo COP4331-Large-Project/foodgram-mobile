c4fb3db376f2861a7394f1a522df9dc2bda8d1eb
DauMike <migueldautant17@gmail.com>
2022-03-22 20:15:26 -0400

    Starting

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { normalizeRect } from 'react-native/Libraries/StyleSheet/Rect';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>We Love COP4331!!!</Text>
+      <View style={styles.title}> 
+        <Text> FoodGram </Text>
+      </View>
+      <TextInput style={styles.input} placeholder={"Username"}/>
+      <TextInput style={styles.input} placeholder={"Password"}/>
+      <Button title='Login' onPress={ () => Alert.alert('Is it working?')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    backgroundColor: '#fff',
+    fontWeight: '700',
+    tintColor: '#3F3D56',
+    fontFamily: 'Prompt',
+    fontStyle: 'normal',
+    fontSize: 24,
+    textAlign: 'center',
+    lineHeight: 36.29,
+
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
