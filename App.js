86af23fa6177202bdb8d1f4e96dd9e11ec384078
bet93 <63423889+bet93@users.noreply.github.com>
2022-03-22 23:46:29 -0400

    Login Screen Update

--- App.js
+++ App.js
@@ -1,43 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 import { normalizeRect } from 'react-native/Libraries/StyleSheet/Rect';
+import React from "react";
+import { View } from "react-native";
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.title}> 
-        <Text> FoodGram </Text>
-      </View>
-      <TextInput style={styles.input} placeholder={"Username"}/>
-      <TextInput style={styles.input} placeholder={"Password"}/>
-      <Button title='Login' onPress={ () => Alert.alert('Is it working?')} />
-      <StatusBar style="auto" />
-    </View>
+    <LoginScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    backgroundColor: '#fff',
-    fontWeight: '700',
-    tintColor: '#3F3D56',
-    fontFamily: 'Prompt',
-    fontStyle: 'normal',
-    fontSize: 24,
-    textAlign: 'center',
-    lineHeight: 36.29,
+// Miguel's code
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.title}> 
+//         <Text> FoodGram </Text>
+//       </View>
+//       <TextInput style={styles.input} placeholder={"Username"}/>
+//       <TextInput style={styles.input} placeholder={"Password"}/>
+//       <Button title='Login' onPress={ () => Alert.alert('Is it working?')} />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     backgroundColor: '#fff',
+//     fontWeight: '700',
+//     tintColor: '#3F3D56',
+//     fontFamily: 'Prompt',
+//     fontStyle: 'normal',
+//     fontSize: 24,
+//     textAlign: 'center',
+//     lineHeight: 36.29,
+
+//   },
+//   input: {
+//     height: 40,
+//     margin: 12,
+//     borderWidth: 1,
+//     padding: 10,
+//   },
+// });
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+
+function LoginScreen(props) {
+    return (
+      <ImageBackground 
+        style={styles.background}
+        source={require("../assets/background_dot.png")}
+      >
+        <View style={styles.logoContainer}>
+          <Text style={styles.title}>FoodGram</Text>
+          <Image style={styles.logo} source={require("../assets/logo.png")}></Image>
+        </View>
+        <View style={styles.loginButton}></View>
+        <View style={styles.registerButton}></View>
+      </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  title: {
+    color: "#3F3D56",
+    fontWeight: "700",
+    fontSize: 30,
+    fontStyle: "normal",
+  },
+  logo: {
+    width: 480,
+    height: 501,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default LoginScreen;
