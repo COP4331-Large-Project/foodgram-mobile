1d8de92dc96bc262a7dcb08609ecf9e4303e2de2
bet93 <63423889+bet93@users.noreply.github.com>
2022-04-15 01:31:34 -0400

    update

--- src/screens/ResetPasswordScreen.js
+++ src/screens/ResetPasswordScreen.js
@@ -6,17 +6,52 @@ import Header from '../components/Header'
 import TextInput from '../components/TextInput'
 import Button from '../components/Button'
 import { emailValidator } from '../helpers/emailValidator'
+import LoginScreen from './LoginScreen'
 
-export default function ResetPasswordScreen({ navigation }) {
+export default function ResetPasswordScreen({ navigation }) 
+{
   const [email, setEmail] = useState({ value: '', error: '' })
+  const [message, setMessage] = useState('')
 
-  const sendResetPasswordEmail = () => {
+  const sendResetPasswordEmail = async event => 
+  {
     const emailError = emailValidator(email.value)
+
     if (emailError) {
       setEmail({ ...email, error: emailError })
       return
     }
-    navigation.navigate('LoginScreen')
+
+    event.preventDefault();
+
+    console.log(email.value);
+    var obj = {Email : email.value};
+    var js = JSON.stringify(obj);
+    try
+    {    
+      const response = await fetch('https://foodgram-demo.herokuapp.com/api/forgotpassword',
+      {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+      var res = JSON.parse(await response.text());
+      //console.log(res.id);
+      if( res.id <= 0 || res.id == undefined)
+      {
+          setMessage(json.error);
+      }
+      else
+      {
+          //console.log(res.id);
+          var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+          //localStorage.setItem('user_data', JSON.stringify(user));
+          setMessage('SUCCESS');
+          console.log(message);
+          //navigation.navigate(LoginScreen);
+      }
+    }
+    catch(e)
+    {
+        console.log(e.toString());
+        return;
+    }
   }
 
   return (
@@ -42,7 +77,7 @@ export default function ResetPasswordScreen({ navigation }) {
         onPress={sendResetPasswordEmail}
         style={{ marginTop: 16 }}
       >
-        Send Instructions
+        Submit
       </Button>
     </Background>
   )
